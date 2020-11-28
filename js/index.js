5b66ec6e144a793e2a56caa5b5ea40ad2f5f7b4c
Nenye <chinenyediei@gmail.com>
2020-11-28 17:19:07 -0600

    random # is generating to the console

--- js/index.js
+++ js/index.js
@@ -1,24 +1,25 @@
+let player1Score = 0;
+let player2Score = 0;
+let player1Turn = true; 
 
 
-function rollDice(min, max) {
-    let playScore = document.getElementsByClassName('playerScore');
-    //clear div
-    let score1 = playScore[0];
-    let score2 = playScore[1];
-    score1.style.display = 'none'; 
-    score2.style.display = 'none'; 
-    //generate rolled number
-    let randomNum = Math.floor(Math.random() * 6) + 1;
-    score1.textContent = randomNum; 
-    score1.style.display = "block";  
-}
-  
-document.getElementById('rollBtn').addEventListener("click", function () {
-    rollDice();
-    //update #rolled on dice
-    
-});
-   
+//store values from DOM nodes
+const player1Dice = document.getElementById("player1Dice");
+const player2Dice = document.getElementById("player2Dice");
+const player1Scoreboard = document.getElementById("player1Scoreboard");
+const playerScoreboard = document.getElementById("player2Scoreboard");
+const message = document.getElementById("message");
+const rollBtn = document.getElementById("rollBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+//random number
+let random = Math.floor(Math.random() * 6) + 1 //the + 1 makes it so its not 0.
+console.log(random);
+//update playerClick
+//check win/loss
+//swithc player
+//update playerClick
+//check win/loss
 
 
 
