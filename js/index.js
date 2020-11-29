e5c8f5adc74e8cef043af3b296b6713d5aa4fbbb
Nenye <chinenyediei@gmail.com>
2020-11-28 18:49:35 -0600

    numbers apending correctly based on rolls, header changes to current player on click. Next need to update the score to hold a total score variable

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let player1Score = 0;
 let player2Score = 0;
 let player1Turn = true; 
-
+let currentPlayer = document.getElementById("currentPlayer"); 
 
 //store values from DOM nodes
 const player1Dice = document.getElementById("player1Dice");
@@ -13,13 +13,27 @@ const rollBtn = document.getElementById("rollBtn");
 const resetBtn = document.getElementById("resetBtn");
 
 //random number
-let random = Math.floor(Math.random() * 6) + 1 //the + 1 makes it so its not 0.
-console.log(random);
-//update playerClick
-//check win/loss
-//swithc player
-//update playerClick
-//check win/loss
+ //the + 1 makes it so its not 0.
 
+//click event listener
+rollBtn.addEventListener('click', function(){
+    const random = Math.floor(Math.random() * 6) + 1
+    
+    if(player1Turn){
+        console.log("Player 1 rolled a " + random)
+        player1Dice.textContent = random;
+        currentPlayer.textContent = "It's Player 2's turn";
+    }
+    else{
+        console.log("Player 2 rolled a " + random)
+        player2Dice.textContent = random;
+        currentPlayer.textContent = "It's Player 1's turn";
+    }
+    player1Turn = !player1Turn
+})
+  
+     // 1. Find out which players turn it is
+     // 2. log out the value e.g. "Player 1 rolled 5"
+     // 3. Switch the turn back to the other player
 
 
